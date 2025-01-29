b816458bb459d75f84d0dc6862c454201cb949c9
adrianallanga <rojas-escobar@hotmail.com>
2025-01-29 19:45:08 +0000

    cambios finales

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,17 @@ import Footer from "./footer";
 import Navbar from "./navbar";
 import Card from "./card";
 import Jumbotrom from "./jumbotrom";
-import Test from "./test";
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
 			<Navbar/>
-			<Test/>
 			<Jumbotrom/>
-			<div className="d-flex">
-				<Card/>
-				<Card/>
-				<Card/>
-				<Card/>
+			<div className="d-flex justify-content-center"> 
+				<Card className="mx-5"/>
+				<Card className="mx-5"/>
+				<Card className="mx-5"/>
+				<Card className="mx-5"/>
 			</div>
 		    <Footer/>
 			<h1>
